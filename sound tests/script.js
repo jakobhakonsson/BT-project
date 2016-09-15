72e9d85a3a0554afb489a0028ecb27026c40f870
Mårten Friman <Twism08@hotmail.com>
2016-09-15 12:25:51 +0200

    Fixed github frakkup

--- sound tests/script.js
+++ sound tests/script.js
@@ -29,31 +29,7 @@ timerAdjustment();
 
 var coughs = ['assets/1.mp3', 'assets/2.wav', 'assets/3.wav'];
 
-function timerAdjustment() {
-  console.log("hej")
-  if (amountOfInfectedNearby == 0) {
-    clearTimeout(timer);
-    var updateTimer = setTimeout(timerAdjustment, 1000);
-    console.log("bajs")
-  } else {
-    timerInterval = Math.max(10000/amountOfInfectedNearby, 3000);
-    var timer = setTimeout(timerAdjustment, timerInterval);
-    clearTimeout(updateTimer);
-    playCough();
-  }
-}
-
 timerAdjustment();
-
-Math.floor(Math.random() * 6) + 1
-
-var coughs = ["assets\coughs\1.mp3", "assets\coughs\2.mp3", "assets\coughs\3.mp3"];
-//var audio = new Audio('cough1.ogg');
-
-function infect() {
-  amountOfInfectedNearby++;
-  console.log("INFECTED")
-}
 
 function infect() {
   amountOfInfectedNearby++;
